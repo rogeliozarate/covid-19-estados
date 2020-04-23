474bbdb6357af31f64a45e77bbce364b61e3e88e
Rogelio Zarate <rogelio@rogeliozarate.com>
2020-04-23 12:25:43 -0500

    Proof of concept
    Starting with the first state.
    clj-http doesn't work fine with enlive html resource, sorted to
    java.net.
    -----
    Now playing Threads by Portishead

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :main ^:skip-aot covid-19-estados.core
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [nrepl "0.7.0"]
+                 [clj-http "3.10.1"]
+                 [enlive "1.1.6"]]
+   :main ^:skip-aot covid-19-estados.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/covid_19_estados/core.clj
+++ src/covid_19_estados/core.clj
@@ -1,7 +1,32 @@
 (ns covid-19-estados.core
-  (:gen-class))
+  (:require [clj-http.client :as client]
+            [net.cgrand.enlive-html :as html]
+            )
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  )
+
+;(defn -main
+;  "I don't do a whole lot ... yet."
+;  [& args]
+;  (println "Hello, World!"))
+
+(defn fetch
+  "Fetchs the webpage"
+  [url]
+  (java.net.URL. url)
+  )
+
+(defn aguascalientes
+  "Aguascalientes have a well formed webpage with the data inside spans with id."
+  []
+  (let [source (html/html-resource(fetch "https://www.aguascalientes.gob.mx/coronavirus/"))
+        resultados {:clave-entidad "1"
+                    :entidad "aguscalientes"
+                    :descartados (first(:content(first(html/select source [:#C-descartados]))))
+                    :sospechosos (first(:content(first(html/select source [:#C-sospechosos]))))
+                    :confirmados (first(:content(first(html/select source [:#C-confirmados]))))
+                    :fallecidos  (first(:content(last(html/select  source [:#C-descartados]))))
+                    }        
+        ]
+    resultados)
+  )
